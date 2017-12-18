6403636f7e9bcf6efc208925b2b0d9eb382c16e9
zeddisarjay24 <34656422+zeddisarjay24@users.noreply.github.com>
2017-12-19 01:25:59 +0800

    files upload

---
+++ VirtualShop/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public Slider audioVolume;
+
+	// Use this for initialization
+	void Start () {
+        audioVolume.value = audioSource.volume;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+	}
+
+    public void adjustVolume() {
+        audioSource.volume = audioVolume.value;
+    }
+}
